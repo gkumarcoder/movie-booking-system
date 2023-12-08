d5641a75c06f4803e33720d509be3638ea17620c
Gaurav Kumar <gkumar4@outlook.com>
2023-12-08 14:19:57 +0530

    added code for Book movie tickets by selecting a theatre, timing, and preferred seats for the day

--- src/main/java/com/movie/booking/controller/MovieBookingController.java
+++ src/main/java/com/movie/booking/controller/MovieBookingController.java
@@ -1,7 +1,9 @@
 package com.movie.booking.controller;
 
+import com.movie.booking.entity.Movie;
 import com.movie.booking.entity.ShowTiming;
 import com.movie.booking.entity.Theatre;
+import com.movie.booking.service.MovieService;
 import com.movie.booking.service.ShowTimingService;
 import com.movie.booking.service.TheatreService;
 import java.time.LocalDateTime;
@@ -10,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,9 @@ public class MovieBookingController {
 
   @Autowired
   private ShowTimingService showTimingService;
+
+  @Autowired
+  private MovieService movieService;
 
   @GetMapping("/theatres")
   public ResponseEntity<List<Theatre>> getAllTheatres() {
@@ -38,6 +45,24 @@ public class MovieBookingController {
     LocalDateTime chosenDate = LocalDateTime.parse(date);
     List<ShowTiming> shows = showTimingService.getShowTimings(chosenDate, movieTitle);
     return new ResponseEntity<>(shows, HttpStatus.OK);
+  }
+
+  @PostMapping("/{movieId}/book")
+  public ResponseEntity<String> bookSeats(
+      @PathVariable Long movieId,
+      @RequestParam int numSeats) {
+
+    try {
+      movieService.bookSeats(movieId, numSeats);
+      return ResponseEntity.ok("Seats booked successfully!");
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @GetMapping
+  public List<Movie> getAllMovies() {
+    return movieService.getAllMovies();
   }
 
 }
